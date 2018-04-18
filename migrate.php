102a504de096042ce40eaa359eb6fdefacf74b33
peaceman <nixalio@gmail.com>
2018-04-18 21:50:55 +0200

    Add migrator class

--- migrate.php
+++ migrate.php
@@ -4,6 +4,11 @@ require_once __DIR__ . '/vendor/autoload.php';
 (new \Dotenv\Dotenv(__DIR__))->load();
 
 use Illuminate\Database\Capsule\Manager as DB;
+use Monolog\Formatter\JsonFormatter;
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Logger;
+
+// setup the database connection
 $capsule = new DB();
 
 $capsule->addConnection([
@@ -17,11 +22,16 @@ $capsule->addConnection([
     'prefix' => '',
 ]);
 
-$capsule->setAsGlobal();
+// setup the logger
+$logger = new Logger('shopware-sku-mapping');
+$logger->pushHandler(
+    (new RotatingFileHandler(__DIR__ . '/logs/le-grand-mappening'))
+        ->setFormatter(new JsonFormatter())
+);
+
+$logger->pushHandler((new \Monolog\Handler\StreamHandler('php://stdout')));
 
 $skuMapping = json_decode(file_get_contents(__DIR__ . '/sku-correction-mapping.json'), true);
 
-DB::table('s_order_details')->limit(10)->orderBy('id')->each(function ($sOrderDetails) {
-    dd($sOrderDetails);
-});
-
+$migrator = new Migrator($capsule->getConnection(), $logger, env('DRY_RUN', true));
+$migrator($skuMapping);
---
+++ src/Migrator.php
@@ -0,0 +1,101 @@
+<?php
+
+use Illuminate\Support\Collection;
+
+class Migrator
+{
+    /**
+     * @var \Illuminate\Database\Connection
+     */
+    protected $db;
+
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @var bool
+     */
+    protected $dryRun;
+
+    public function __construct(\Illuminate\Database\Connection $db, \Psr\Log\LoggerInterface $logger, bool $dryRun)
+    {
+        $this->db = $db;
+        $this->logger = $logger;
+        $this->dryRun = $dryRun;
+    }
+
+    public function __invoke(array $skuMapping)
+    {
+        $this->logger->info('Start mapping', ['dryRun' => $this->dryRun]);
+
+        $skuMapping = $this->enrichSKUMappingWithNewArticlesIds($skuMapping);
+        $skuMapping = $skuMapping->mapWithKeys(function ($newData, $oldSKU) {
+            return [strtolower($oldSKU) => $newData];
+        });
+
+        $oldSKUs = $skuMapping->keys()->map(function ($sku) { return (string)$sku; });
+
+        $this->db->beginTransaction();
+
+        $sOrderDetailsQuery = $this->db->table('s_order_details')
+            ->whereIn('articleordernumber', $oldSKUs)
+            ->select(['id', 'articleordernumber', 'articleID']);
+
+        $sOrderDetailsQuery->get()->each(function ($sOrderDetails) use ($skuMapping) {
+            $newData = $skuMapping->get(strtolower($sOrderDetails->articleordernumber));
+
+            if (!$newData) {
+                $this->logger->warning('Failed to find matching new data, skipping', [
+                    'sOrderDetailsId' => $sOrderDetails->id, 'oldSKU' => $sOrderDetails->articleordernumber
+                ]);
+                return;
+            }
+
+            $this->logger->info('Updating s_order_details', [
+                'sOrderDetailsId' => $sOrderDetails->id,
+                'oldSKU' => $sOrderDetails->articleordernumber, 'newSKU' => $newData['sku'],
+                'oldArticleId' => $sOrderDetails->articleID, 'newArticleId' => $newData['sArticlesId']
+            ]);
+
+            $this->db->table('s_order_details')
+                ->where('id', $sOrderDetails->id)
+                ->update(['articleordernumber' => $newData['sku'], 'articleID' => $newData['sArticlesId']]);
+        });
+
+        $this->dryRun ? $this->db->rollBack(): $this->db->commit();
+    }
+
+    protected function enrichSKUMappingWithNewArticlesIds(array $skuMapping): Collection
+    {
+        return collect($skuMapping)
+            ->map(function (string $sku) {
+                $sArticlesId = $this->getArticlesIdForSKU($sku);
+
+                return ['sku' => $sku, 'sArticlesId' => $sArticlesId];
+            });
+    }
+
+    protected function getArticlesIdForSKU(string $sku)
+    {
+        $sArticles = $this->db->table('s_articles')
+            ->join('s_articles_details', 's_articles_details.articleID', '=', 's_articles.id')
+            ->where('s_articles_details.ordernumber', '=', $sku)
+            ->get(['s_articles.id']);
+
+        if ($sArticles->count() > 1) {
+            $this->logger->warning(
+                'Found more than one s_articles.id per for a SKU',
+                ['sku' => $sku, 'sArticlesIds' => $sArticles->pluck('id')]
+            );
+        }
+
+        $sArticle = $sArticles->first();
+        if (!$sArticle) {
+            $this->logger->warning('Failed to find a new s_articles.id for a SKU', ['sku' => $sku]);
+        }
+
+        return $sArticle ? $sArticle->id : null;
+    }
+}
